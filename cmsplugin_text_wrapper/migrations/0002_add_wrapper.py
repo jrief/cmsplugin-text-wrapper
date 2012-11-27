0588cf4e3e4f7dbfb0eb95e7069463f07e152c7b
Jacob Rief <jacob.rief@gmail.com>
2012-11-27 12:14:31 +0100

    Fixed problem if table cmsplugin_text did not exist already. Bumps to version 0.3.

---
+++ cmsplugin_text_wrapper/migrations/0002_add_wrapper.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        db.add_column('cmsplugin_text', 'wrapper',
+              self.gf('django.db.models.fields.CharField')(default='', max_length=50, blank=True),
+              keep_default=False)
+
+    def backwards(self, orm):
+        db.delete_column('cmsplugin_text', 'wrapper')
+
+#    models = {
+#        'cmsplugin_text.text': {
+#            'Meta': {'db_table': "'cmsplugin_text'"},
+#            'body': ('models.TextField', [], {}),
+#            'wrapper': ('models.CharField', [], {'max_length': '50', 'blank': True}),
+#            'cmsplugin_ptr': ('models.OneToOneField', [], {'to': "orm['cms.CMSPlugin']", 'unique': 'True', 'primary_key': 'True'})
+#        },
+#    }
+
+    models = {
+        'cms.cmsplugin': {
+            'Meta': {'object_name': 'CMSPlugin'},
+            'changed_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'creation_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2012, 11, 27, 0, 0)'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'language': ('django.db.models.fields.CharField', [], {'max_length': '15', 'db_index': 'True'}),
+            'level': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'lft': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.CMSPlugin']", 'null': 'True', 'blank': 'True'}),
+            'placeholder': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.Placeholder']", 'null': 'True'}),
+            'plugin_type': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'}),
+            'position': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'rght': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'})
+        },
+        'cms.placeholder': {
+            'Meta': {'object_name': 'Placeholder'},
+            'default_width': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'slot': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'})
+        },
+        'cmsplugin_text_wrapper.text': {
+            'Meta': {'object_name': 'Text', 'db_table': "'cmsplugin_text'"},
+            'body': ('django.db.models.fields.TextField', [], {}),
+            'cmsplugin_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['cms.CMSPlugin']", 'unique': 'True', 'primary_key': 'True'})
+        }
+    }
+
+    complete_apps = ['cmsplugin_text_wrapper']
